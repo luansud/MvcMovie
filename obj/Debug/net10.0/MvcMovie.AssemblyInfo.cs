dc04acc7f2c5343ba1ce05a2041c734244ff6ba8
luansud <luanfmedeiros@hotmail.com>
2026-02-03 12:25:46 -0700

    Commit before add the filter by year, (Everything beside that is ready)

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56429111159992ebcca6dc5f9eeb6ee961c71d1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f06794ec2d7add3962ff600e24b6bc4cad0da9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
