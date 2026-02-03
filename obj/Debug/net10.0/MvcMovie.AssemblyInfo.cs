796a1b63674a8fa8a79bc69d2ea35d1d094b7036
luansud <luanfmedeiros@hotmail.com>
2026-02-03 13:40:16 -0700

    Uploading video

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2095f03fa7480e939202a9ed6a157a0f01140b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dc0e20e6d4a8f77e6c66cd0be2033301bc4f954")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
